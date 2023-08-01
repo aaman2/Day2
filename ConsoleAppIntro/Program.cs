76f9f870e8618ca731a0d451395021068c62becf
raisircl <rai.verma@gmail.com>
2023-08-01 10:59:27 +0530

    Add project files.

---
+++ ConsoleAppIntro/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    //static void Main()
+    //{
+    //    System.Console.WriteLine("Hello User");
+    //    System.Console.WriteLine("Welcome in C#.Net");
+    //}
+}
+//System is a namespace (Collection of classes and it is a logical name which is given to set of classes)
+//ctrl + k + c => comment
+//ctrl + k + u => uncomment
---
+++ ConsoleAppIntro/Sum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIntro
+{
+    internal class Sum
+    {
+        static void Main()
+        {
+            int n1, n2, total;
+            Console.WriteLine("enter 1st number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter 2nd number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            total = n1 + n2;
+            Console.WriteLine($"The sum is {total}"); //$"" (string interpolation)
+            Console.WriteLine($"The sum of {n1} and {n2} is {total}");
+
+        }
+    }
+}
+/*
+ 
+ -
+ *
+ /
+ % 
+ 
+ */
