65469aced5814efb92f3c6471e1781a731ee4a6e
raisircl <rai.verma@gmail.com>
2023-08-07 10:20:19 +0530

    rd account

---
+++ ConsoleAppIntro/RD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIntro
+{
+    internal class RD
+    {
+        static void Main()
+        {
+            int premium,  fives;
+            double maturity;
+            Console.WriteLine("enter premium multiple of 5");
+            premium = Convert.ToInt32(Console.ReadLine());
+
+            fives = premium / 5;
+
+            maturity = fives * 364.27;
+
+            Console.WriteLine($"Maturity of RD with {premium} per month is {maturity} after 5 Years.");
+
+        }
+    }
+}
