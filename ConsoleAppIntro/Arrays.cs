18aff8617c9777e78057891fa0c7cfc5b45067c2
raisircl <rai.verma@gmail.com>
2023-08-10 10:27:28 +0530

    array and matrix

---
+++ ConsoleAppIntro/Arrays.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleAppIntro
+//{
+//    internal class Arrays
+//    {
+//        static void Main()
+//        {
+//            // collection of multiple values of same type
+//            //int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+//            //for(int i = 0; i < a.Length; i++)
+//            //{
+//            //    Console.WriteLine(a[i]);
+//            //}
+
+//            int[] b=new int[5];
+//            int i,sum=0;
+//            for(i=0;i<5;i++)
+//            {
+//                Console.WriteLine("Enter element:");
+//                b[i]=Convert.ToInt32(Console.ReadLine());   
+//            }
+//            Console.WriteLine("\nYour array elements are:");
+//            for (i = 0; i < 5; i++)
+//            {
+//                sum = sum + b[i];
+//                Console.WriteLine(b[i]);
+//            }
+//            Console.WriteLine($"The sum of array elements is {sum}");
+//            Console.WriteLine("Array in reverse order");
+//            for(i = 4; i >=0;i--)
+//            {
+//                Console.WriteLine(b[i]);
+//            }
+//            /*
+//             1. Search and element
+//             2. Print Biggest element
+//             3. sort array elements
+//             */
+
+//        }
+//    }
+//}
---
+++ ConsoleAppIntro/MyMatrix.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIntro
+{
+    internal class MyMatrix
+    {
+        static void Main()
+        {
+            int[,] a = new int[3, 3];
+            int i, j;
+            for(i=0;i<3;i++)
+            {
+                for(j=0;j<3;j++)
+                {
+                    Console.WriteLine("enter element:");
+                    a[i,j]=Convert.ToInt32(Console.ReadLine()); 
+                }
+            }
+            Console.WriteLine("Your matrix elements are:");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+
+                    Console.Write($"{a[i,j]}\t");  
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Your matrix transpose elements are:");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+
+                    Console.Write($"{a[j, i]}\t");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Your matrix left diagonal elements are:");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                        Console.Write($"{a[j, i]}\t");
+                    else
+                        Console.Write("\t");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Your matrix right diagonal elements are:");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (i +j==2)
+                        Console.Write($"{a[j, i]}\t");
+                    else
+                        Console.Write("\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+/*
+ 1. lower triangle 
+ 2. upper triangle
+ 3. sum of 2 matrix
+ 4. product of 2 matrix
+ 
+ */
